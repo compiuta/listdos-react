3b7f25ec0ff3d1b7c055b58d11cc59e98e72c5e9
compiuta <christianr@rocketmail.com>
2021-06-16 15:18:04 -0700

    add useState and useEffect hooks to fetch and set localStorage data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Todo from "./Todo";
 
 const testData = [
@@ -20,9 +20,27 @@ const testData = [
 ];
 
 const App = () => {
-  return testData.map((data) => {
-    return <Todo key={data.id} {...data} />;
-  });
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    if (!localStorage.hasOwnProperty("listdos")) {
+      localStorage.setItem("listdos", JSON.stringify(testData));
+    }
+
+    setData(JSON.parse(localStorage.getItem("listdos")));
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <div className={isLoading ? "loading container" : "container"}>
+      {data.map((data) => {
+        return <Todo key={data.id} {...data} />;
+      })}
+    </div>
+  );
 };
 
 export default App;
