b99534871a126fd564320934121404282c0f45c7
compiuta <christianr@rocketmail.com>
2021-06-18 14:40:29 -0700

    add edit funcionality

--- src/Todo.js
+++ src/Todo.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todo = ({ id, todo, complete, allData, sendData }) => {
+  const [isEditMode, setIsEditMode] = useState(false);
+  const [editInputValue, setEditInputValue] = useState(todo);
   const deleteTodo = () => {
     const filteredData = allData.filter((item) => item.id !== id);
 
     sendData(filteredData);
   };
+
+  const toggleEditMode = () => {
+    setIsEditMode(!isEditMode);
+  };
+
+  const handleEditInputValue = (e) => {
+    setEditInputValue(e.target.value);
+  };
+
+  const editTodo = () => {
+    const editedData = {
+      id: id,
+      todo: editInputValue,
+      complete: complete,
+    };
+
+    const newData = allData.map((item) =>
+      item.id === editedData.id ? editedData : item
+    );
+    sendData(newData);
+    toggleEditMode();
+  };
   return (
-    <div className="todo-container">
-      <h4 className="todo">
-        {id} {todo}
-      </h4>
+    <div className={isEditMode ? "todo-container edit-mode" : "todo-container"}>
+      {isEditMode ? (
+        <input
+          type="text"
+          value={editInputValue}
+          onChange={handleEditInputValue}
+        />
+      ) : (
+        <h4 className="todo">
+          {id} {todo}
+        </h4>
+      )}
+
       <p className={complete ? "complete" : "not-complete"}>
         {complete ? "complete" : "not-complete"}
       </p>
@@ -18,7 +51,15 @@ const Todo = ({ id, todo, complete, allData, sendData }) => {
         <button className="todo-delete" onClick={deleteTodo}>
           delete
         </button>
-        <button className="todo-edit">edit</button>
+        {isEditMode ? (
+          <button className="done" onClick={editTodo}>
+            done
+          </button>
+        ) : (
+          <button className="todo-edit" onClick={toggleEditMode}>
+            edit
+          </button>
+        )}
       </div>
     </div>
   );
