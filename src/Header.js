f5f18a43f00070ffb4d85e6fedb91dda08bcb056
compiuta <christianr@rocketmail.com>
2021-06-20 09:09:23 -0700

    add header component and start css coding using sass

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="main-header">
+      <span className="logo">logo</span>
+      <span className="header-category active">all</span>
+    </header>
+  );
+};
+
+export default Header;
