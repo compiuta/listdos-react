ebd3546d5efa1a26a1b27d4f34fea15e754375fd
compiuta <christianr@rocketmail.com>
2021-06-24 10:42:17 -0700

    refactor tasklist code and add individual page files

--- src/Home.js
+++ src/Home.js
@@ -1,98 +1,14 @@
-import React, { useState, useEffect } from "react";
-import Task from "./Task";
+import React from "react";
+import TaskList from "./TaskList";
 import fish from "./fish.png";
 const Home = () => {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [taskValue, setTaskValue] = useState("");
-
-  const fetchData = () => {
-    const parsedData = JSON.parse(localStorage.getItem("listdos"));
-    setData(parsedData);
-    console.log(parsedData);
-    setIsLoading(false);
+  const info = {
+    listType: "tasksToComplete",
+    listCompleted: false,
+    message: "No tasks yet",
+    img: fish,
   };
-
-  const sendData = (newData) => {
-    console.log("sending");
-    localStorage.setItem("listdos", JSON.stringify(newData));
-    setData(newData);
-    setIsLoading(false);
-  };
-
-  const addTask = () => {
-    console.log("add");
-    const newTask = {
-      id: Date.now() + Math.random(),
-      task: taskValue,
-      complete: false,
-    };
-
-    const newData = { ...data };
-    console.log(newData);
-    newData.tasksToComplete++;
-    console.log(`newtocompletee ${newData.tasksToComplete}`);
-    newData.data.push(newTask);
-    sendData(newData);
-
-    setTaskValue("");
-  };
-
-  const taskInputChange = (e) => {
-    setTaskValue(e.target.value);
-  };
-
-  useEffect(() => {
-    if (!localStorage.hasOwnProperty("listdos")) {
-      const initialData = {
-        tasksToComplete: 0,
-        completedTasks: 0,
-        data: [],
-      };
-      localStorage.setItem("listdos", JSON.stringify(initialData));
-    }
-
-    fetchData();
-  }, []);
-  return (
-    <div className={isLoading ? "loading tasks-container" : "tasks-container"}>
-      <textarea
-        className="task-textarea"
-        rows="2"
-        value={taskValue}
-        onChange={taskInputChange}
-      ></textarea>
-      <button
-        className="btn add-task"
-        onClick={addTask}
-        disabled={taskValue.replace(/ /g, "") ? "" : "disabled"}
-      >
-        Add Task
-      </button>
-      {data.tasksToComplete > 0 ? (
-        <div className="listdos">
-          {data.data.map((item) => {
-            if (!item.complete) {
-              return (
-                <Task
-                  key={item.id}
-                  {...item}
-                  allData={data}
-                  sendData={sendData}
-                />
-              );
-            }
-            return;
-          })}
-        </div>
-      ) : (
-        <div className="no-tasks">
-          <h4>No tasks yet</h4>
-          <img src={fish} alt="fish" className="img-responsive" />
-        </div>
-      )}
-    </div>
-  );
+  return <TaskList {...info} />;
 };
 
 export default Home;
---
+++ src/TaskList.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+import Task from "./Task";
+const TaskList = ({ listType, listCompleted, message, img }) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [taskValue, setTaskValue] = useState("");
+
+  const fetchData = () => {
+    const parsedData = JSON.parse(localStorage.getItem("listdos"));
+    setData(parsedData);
+    console.log(parsedData);
+    setIsLoading(false);
+  };
+
+  const sendData = (newData) => {
+    console.log("sending");
+    localStorage.setItem("listdos", JSON.stringify(newData));
+    setData(newData);
+    setIsLoading(false);
+  };
+
+  const addTask = () => {
+    console.log("add");
+    const newTask = {
+      id: Date.now() + Math.random(),
+      task: taskValue,
+      complete: false,
+    };
+
+    const newData = { ...data };
+    console.log(newData);
+    newData.tasksToComplete++;
+    console.log(`newtocompletee ${newData.tasksToComplete}`);
+    newData.data.push(newTask);
+    sendData(newData);
+
+    setTaskValue("");
+  };
+
+  const taskInputChange = (e) => {
+    setTaskValue(e.target.value);
+  };
+
+  useEffect(() => {
+    if (!localStorage.hasOwnProperty("listdos")) {
+      const initialData = {
+        tasksToComplete: 0,
+        completedTasks: 0,
+        data: [],
+      };
+      localStorage.setItem("listdos", JSON.stringify(initialData));
+    }
+
+    fetchData();
+  }, []);
+  return (
+    <div className={isLoading ? "loading tasks-container" : "tasks-container"}>
+      <textarea
+        className="task-textarea"
+        rows="2"
+        value={taskValue}
+        onChange={taskInputChange}
+      ></textarea>
+      <button
+        className="btn add-task"
+        onClick={addTask}
+        disabled={taskValue.replace(/ /g, "") ? "" : "disabled"}
+      >
+        Add Task
+      </button>
+      {data[listType] > 0 ? (
+        <div className="listdos">
+          {data.data.map((item) => {
+            if (item.complete === listCompleted) {
+              return (
+                <Task
+                  key={item.id}
+                  {...item}
+                  allData={data}
+                  sendData={sendData}
+                />
+              );
+            }
+            return;
+          })}
+        </div>
+      ) : (
+        <div className="no-tasks">
+          <h4>{message}</h4>
+          <img src={img} alt="fish" className="img-responsive" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TaskList;
